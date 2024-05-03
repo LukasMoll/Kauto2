d96ab0686c9d068b2a6139a460b59f6d1ec0e700
lukas moll <lukasmolleman@gmaill.com>
2024-05-03 13:45:41 +0000

    Added stuff to requirements and fixed analysis and wrote README.md

--- analysis/generate_kotlin_line_completion.py
+++ analysis/generate_kotlin_line_completion.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 import json
 
@@ -10,23 +11,32 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, AutoMo
 
 import random
 
+
+parser = argparse.ArgumentParser(description='Script to generate kotlin code for line completion based on eval data')
+
+# Add two string arguments
+parser.add_argument('model_id', type=str, help='model id')
+parser.add_argument('fine_tuned_model_id', type=str, help='fine tuned model id')
+args = parser.parse_args()
+
+
+model_id = args.model_id
+fine_tuned_model_id = args.fine_tuned_model_id
+
 torch.set_default_device("cuda")
 
-model_name = "microsoft/Phi-3-mini-4k-instruct"
-
 # Load model and tokenizer from Hugging Face Hub
-model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", trust_remote_code=True)
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype="auto", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = 'left'
 
 # Correctly initialize the PeftModel
-fine_tuned_model_path = "../fine_tuning/phi-3_kotlin_fine_tuned"
+fine_tuned_model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype="auto", trust_remote_code=True)
 adapter_name = "personal_copilot"
 
 # Load the model with correct configuration setup
 # Make sure that the configuration being passed does not include unexpected arguments.
-fine_tuned_model = PeftModel.from_pretrained(model, fine_tuned_model_path, adapter_name=adapter_name)
+fine_tuned_model = PeftModel.from_pretrained(fine_tuned_model, fine_tuned_model_id, adapter_name=adapter_name)
 
 # Additional operations
 fine_tuned_model.add_weighted_adapter(["personal_copilot"], [0.8], "best_personal_copilot")
@@ -36,7 +46,7 @@ fine_tuned_model.set_adapter("best_personal_copilot")
 random.seed(10)
 
 
-df = pd.read_parquet("../dataset_parser/train_data/kotlin_train_data.parquet")
+df = pd.read_parquet("../dataset_parser/eval_data/kotlin_eval_data2.parquet")
 file_name = "kotlin_line_completion_results.csv"
 with open(file_name, "w") as file:
     file.write("actual_next_line,predicted_next_line,similarity,"
@@ -44,9 +54,9 @@ with open(file_name, "w") as file:
 
 
 def predict_next_line(upper_half, model2):
-    inputs = tokenizer(upper_half, max_length=3800, truncation=True, return_tensors="pt", return_attention_mask=False)
+    inputs = tokenizer(upper_half, max_length=1900, truncation=True, return_tensors="pt", return_attention_mask=False)
     input_length = len(inputs[0])
-    outputs = model2.generate(**inputs, max_length=input_length + 50)
+    outputs = model2.generate(**inputs, max_length=input_length + 100)
     text = tokenizer.batch_decode(outputs)[0]
     result = text.splitlines()[upper_half.count("\n")]
     if text.count("\n") == upper_half.count("\n"):
@@ -59,8 +69,11 @@ exact_matches = 0
 exact_matches_fine_tuned = 0
 similarity_list = []
 similarity_list_fine_tuned = []
+exact_matches_list = []
+exact_matches_list_fine_tuned = []
 upper_halve_to_next_line = []
-for kotlin_code in df["text"]:
+text_len = len(df["text"])
+for i, kotlin_code in enumerate(df["text"]):
     lines = kotlin_code.splitlines()
     if len(lines) < 4:
         continue
@@ -74,16 +87,14 @@ for kotlin_code in df["text"]:
     predicted_next_line_fine_tuned = predict_next_line(upper_half, fine_tuned_model)
     similarity = fuzz.ratio(actual_next_line, predicted_next_line)
     similarity_fine_tuned = fuzz.ratio(actual_next_line, predicted_next_line_fine_tuned)
-    exact_match = actual_next_line == predicted_next_line
-    exact_match_fine_tuned = actual_next_line == predicted_next_line_fine_tuned
-    with open(file_name, "a") as file:
-        file.write(f"{actual_next_line},{predicted_next_line},{similarity},{exact_match},"
-                   f"{predicted_next_line_fine_tuned},{similarity_fine_tuned},{exact_match_fine_tuned}\n")
-    exact_matches += exact_match
-    exact_matches_fine_tuned += exact_match_fine_tuned
+    exact_matches_list.append(actual_next_line == predicted_next_line)
+    exact_matches_list_fine_tuned.append(actual_next_line == predicted_next_line_fine_tuned)
     similarity_list.append(similarity)
     similarity_list_fine_tuned.append(similarity_fine_tuned)
-    print(f"Exact matches            : {exact_matches}")
-    print(f"Exact matches fine tuned : {exact_matches_fine_tuned}")
-    print(f"Similarity               : {round(sum(similarity_list) / len(similarity_list), 2)}")
-    print(f"Similarity fine tuned    : {round(sum(similarity_list_fine_tuned) / len(similarity_list_fine_tuned), 2)}")
+    print(f"{i+1}/{text_len}")
+    print(f"Exact matches             : {round(100 * sum(exact_matches_list) / len(exact_matches_list), 2)}%")
+    print(f"Exact matches fine tuned  : {round(100 * sum(exact_matches_list_fine_tuned) / len(exact_matches_list_fine_tuned), 2)}%")
+    print(f"Similarity                : {round(sum(similarity_list) / len(similarity_list), 2)}%")
+    print(f"Similarity fine tuned     : {round(sum(similarity_list_fine_tuned) / len(similarity_list_fine_tuned), 2)}%")
+    print(f"Exact matches improvement : {round(100 * (sum(exact_matches_list_fine_tuned) / sum(exact_matches_list) - 1), 2)}%")
+    print(f"Similarity improvement    : {round(100 * (sum(similarity_list_fine_tuned) / sum(similarity_list) - 1), 2)}%")
--- analysis/generate_python_method_completion.py
+++ analysis/generate_python_method_completion.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 import json
 
@@ -9,20 +10,32 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, AutoMo
 torch.set_default_device("cuda")
 
 
+parser = argparse.ArgumentParser(description='Script to generate kotlin code for line completion based on eval data')
+
+# Add two string arguments
+parser.add_argument('model_id', type=str, help='model id')
+parser.add_argument('fine_tuned_model_id', type=str, help='fine tuned model id')
+args = parser.parse_args()
+
+
+model_id = args.model_name
+fine_tuned_model_id = args.fine_tuned_model_name
+
+
 # Correctly initialize the PeftModel
-model_id = "microsoft/Phi-3-mini-4k-instruct"
-fine_tuned_path = "../fine_tuning/phi-3_kotlin_fine_tuned"
+# model_id = "microsoft/Phi-3-mini-4k-instruct"
+# fine_tuned_model_path = "LukasM1/phi-3_kotlin_fine_tuned_4k2"
 adapter_name = "personal_copilot"
 
 # Load model and tokenizer from Hugging Face Hub
 model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype="auto", trust_remote_code=True)
 tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = 'left'
 
 # Load the model with correct configuration setup
 # Make sure that the configuration being passed does not include unexpected arguments.
-fine_tuned_model = PeftModel.from_pretrained(model, fine_tuned_path, adapter_name=adapter_name)
+fine_tuned_model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype="auto", trust_remote_code=True)
+fine_tuned_model = PeftModel.from_pretrained(fine_tuned_model, fine_tuned_model_id, adapter_name=adapter_name)
 
 # Additional operations
 fine_tuned_model.add_weighted_adapter([adapter_name], [0.8], "best_personal_copilot")
@@ -38,34 +51,45 @@ def batch_inference(batch_inputs, tokenizer, model, max_length):
     decoded_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
     return decoded_outputs
 
-def get_method_body(generated: str):
-    result = ""
-    for i, line in enumerate(generated.splitlines()):
-        if line and line[0] != " ":
-            break
-        if line:
-            result += line.strip() + " "
-        if line[:10] == "    return" or line[:8] == "  return":
-            break
-    return result.strip()
+def get_method_body(code):
+    lines = code.splitlines()
+    method_body = []
+    current_indent = None
+
+    for line in lines:
+        if current_indent is None:
+            # Determine the initial indentation on the first line of the method body
+            current_indent = len(line) - len(line.lstrip())
+            method_body.append(line.strip())
+        else:
+            # Continue capturing lines that are part of the method body
+            line_indent = len(line) - len(line.lstrip())
+            if line_indent <= current_indent:
+                # If indentation decreases or remains the same, stop capturing
+                break
+            method_body.append(line.strip())
+
+    return " ".join(method_body).strip()
+
+
 
 result = []
-with open('python_test.jsonl') as f:
+with open('test.jsonl') as f:
     for line in f:
         result.append(json.loads(line))
 for i, input in enumerate(result[:100]):
     prompt = input['signature'] + "\n\"\"\"" + input['docstring'] + "\"\"\"\n"
     prompt = tokenizer.decode(tokenizer.encode(prompt, padding=True,
                               return_tensors="pt", truncation=True)[0])
-    inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=False)
-    outputs = model.generate(**inputs, max_length=1024)
-    outputs_fine_tuned = fine_tuned_model.generate(**inputs, max_length=1024)
-    generated = tokenizer.batch_decode(outputs)[0][len(prompt):]
-    generated_fine_tuned = tokenizer.batch_decode(outputs_fine_tuned)[0][len(prompt):]
+    inputs = tokenizer(prompt, max_length=3000, return_tensors="pt", return_attention_mask=False)
+    outputs = model.generate(**inputs, max_length=200 + len(inputs[0]))
+    outputs_fine_tuned = fine_tuned_model.generate(**inputs, max_length=200 + len(inputs[0]))
+    generated = "\n".join(tokenizer.batch_decode(outputs)[0].splitlines()[prompt.count("\n"):])
+    generated_fine_tuned = "\n".join(tokenizer.batch_decode(outputs_fine_tuned)[0].splitlines()[prompt.count("\n"):])
     method_body = get_method_body(generated)
     method_body_fine_tuned = get_method_body(generated_fine_tuned)
-    with open('python_codexglue_results.txt', 'a') as file:
+    with open('normal.txt', 'a') as file:
         file.write(method_body + '\n')
-    with open('python_codexglue_results_fine_tuned.txt', 'a') as file:
+    with open('fine_tuned.txt', 'a') as file:
         file.write(method_body_fine_tuned + '\n')
     print(f"{i+1}/100")
